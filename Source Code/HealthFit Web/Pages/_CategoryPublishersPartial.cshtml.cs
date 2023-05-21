96a36c9c1df0ca4dc7181b72f3a7048a5a0c23c3
Vishal S Bajad <vishal.bajad@gmail.com>
2023-05-21 21:08:10 +0530

    Code Changes

---
+++ Source Code/HealthFit Web/Pages/_CategoryPublishersPartial.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HealthFit_Web.Pages
+{
+    public class CategoryPublishersModel : PageModel
+    {
+        private readonly ILogger<CategoryPublishersModel> _logger;
+
+        public CategoryPublishersModel(ILogger<CategoryPublishersModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnGetPartial() => Partial("Category");
+        public void OnGet()
+        {
+
+        }
+    }
+}
